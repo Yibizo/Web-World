ae8b5f2b17b02cdc0f6d2455f885e5f4681e055d
Yibizo <diegomejiasuarez@gmail.com>
2021-10-28 21:00:11 -0500

    General updates

--- weather_app/src/app.js
+++ weather_app/src/app.js
@@ -9,7 +9,7 @@ const templatesDirectoryPath = path.join(__dirname, '../templates')
 const viewsDirectoryPath = `${templatesDirectoryPath}/views`
 const partialsDirectoryPath = `${templatesDirectoryPath}/partials`
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(templatesDirectoryPath))
 
--- weather_app/templates/js/weather.js
+++ weather_app/templates/js/weather.js
@@ -8,10 +8,14 @@ weatherForm.addEventListener('submit', (e) => {
 
     fetch(`http://localhost:3000/weather/${location}`).then((response) => {
         response.json().then((data) => {
+            const displayElements = [document.getElementById('city'), document.getElementById('description'), document.getElementById('temperature'), document.getElementById('time'), document.getElementById('weatherImg')]
             if (data.error) {
-                document.getElementById('city').textContent = data.error
+                displayElements[0].textContent = data.error
+                displayElements[1].textContent = ''
+                displayElements[2].textContent = ''
+                displayElements[3].textContent = ''
+                displayElements[4].src = ''
             } else {
-                const displayElements = [document.getElementById('city'), document.getElementById('description'), document.getElementById('temperature'), document.getElementById('time'), document.getElementById('weatherImg')]
                 displayElements[0].textContent = data.city
                 displayElements[1].textContent = data.description
                 displayElements[2].textContent = `${data.temperature}°`
